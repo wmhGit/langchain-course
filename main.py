1e0297fd8a9eb8ad0389fd15bb80e20eb310a978
WILLIAM HEALY <williamhealy@WILLIAMs-MacBook-Pro.local>
2026-01-16 13:52:22 -0500

    bla

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from dotenv import load_dotenv
 
+from typing import List
+from pydantic import BaseModel, Field
+
 load_dotenv()
 from langchain.agents import create_agent
 from langchain.tools import tool
@@ -7,11 +10,19 @@ from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
-
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+    url:str = Field(description="The URL of the source")
+    
+class AgentResponse(BaseModel):
+    """ Schema for agent response with answer and sources """
+    
+    answer:str = Field(description="zthe agent's answer to the query")
+    sources: List[Source] = Field(default_factory=list, description="List of sources used to generate the answer")
 
 llm = ChatOpenAI()
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 def main():
     print("Hello from langchain-course!")
     result = agent.invoke({"messages":HumanMessage(content="Search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details")})
