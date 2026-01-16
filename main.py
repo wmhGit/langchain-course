a3b1e69827f11ae549dcca44c008b4b6f8bf49bb
WILLIAM HEALY <williamhealy@WILLIAMs-MacBook-Pro.local>
2026-01-16 15:24:56 -0500

    react search agent

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
 from dotenv import load_dotenv
 
-from typing import List
-from pydantic import BaseModel, Field
-
 load_dotenv()
-from langchain.agents import create_agent
-from langchain.tools import tool
-from langchain_core.messages import HumanMessage
+
+from langchain_classic import hub
+#from langchain.agents import AgentExecutor
+from langchain_classic.agents import AgentExecutor
+#from langchain.agents.react.agent import create_react_agent
+from langchain_classic.agents.react.agent import create_react_agent
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
-class Source(BaseModel):
-    """Schema for a source used by the agent"""
-    url:str = Field(description="The URL of the source")
-    
-class AgentResponse(BaseModel):
-    """ Schema for agent response with answer and sources """
-    
-    answer:str = Field(description="zthe agent's answer to the query")
-    sources: List[Source] = Field(default_factory=list, description="List of sources used to generate the answer")
-
-llm = ChatOpenAI()
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
+llm = ChatOpenAI(model="gpt-4")
+react_prompt = hub.pull("hwchase17/react")
+agent = create_react_agent(
+    llm=llm,
+    tools=tools,
+    prompt=react_prompt,
+)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+chain = agent_executor
+
 def main():
-    print("Hello from langchain-course!")
-    result = agent.invoke({"messages":HumanMessage(content="Search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details")})
+    result = chain.invoke(
+        input={
+            "input": "search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details",
+        }
+    )
     print(result)
 
 
